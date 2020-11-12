82061f2c0fb2a829a8c9fabd5dca9a9ac7631bc7
Lucas Passos Vanderlinde <lucaspvanderlinde@hotmail.com>
2020-11-12 09:02:43 -0300

    Criado o fonte do back-end com Spring Starter e adicinado algumas dependencias.

---
+++ back-end/estudo/src/test/java/com/exemplo/api/EstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exemplo.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
