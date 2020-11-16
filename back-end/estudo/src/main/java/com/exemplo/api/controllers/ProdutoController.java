6e5ceae41d628ade1a07e86beda1d96d3c6a125c
Lucas Passos Vanderlinde <lucaspvanderlinde@hotmail.com>
2020-11-16 20:07:37 -0300

    Desenvolvido os metodos buscarPorId e cadastrar.

--- back-end/estudo/src/main/java/com/exemplo/api/controllers/ProdutoController.java
+++ back-end/estudo/src/main/java/com/exemplo/api/controllers/ProdutoController.java
@@ -1,11 +1,15 @@
 package com.exemplo.api.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,21 +20,43 @@ import com.exemplo.api.services.ProdutoService;
 @RestController
 @RequestMapping(value = "/produto")
 public class ProdutoController {
-	
+
 	@Autowired
 	private ProdutoService produtoService;
-	
-	
+
 	/**
 	 * Lista todos os produtos que estão no banco.
 	 * 
 	 * @return ResponseEntity<List<Produto>>
 	 */
-	
 	@GetMapping
 	public ResponseEntity<List<Produto>> buscarTodos() {
 		List<Produto> lista = produtoService.buscarTodos();
 		return ResponseEntity.ok().body(lista);
+	}
+
+	/**
+	 * Busca um produto que está no banco pelo id.
+	 * 
+	 * @param id
+	 * @return ResponseEntity<Produto>
+	 */
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Optional<Produto>> buscarPorId(@PathVariable Long id) {
+		Optional<Produto> produto = produtoService.buscarPorId(id);
+		return ResponseEntity.ok().body(produto);
+	}
+
+	/**
+	 * Cadastra um produto novo no banco.
+	 * 
+	 * @param produto
+	 * @return ResponseEntity<Produto>
+	 */
+	@PostMapping
+	public ResponseEntity<Produto> cadastrar(@RequestBody Produto produto) {
+		produto = produtoService.cadastrar(produto);
+		return ResponseEntity.ok().body(produto);
 	}
 
 }
