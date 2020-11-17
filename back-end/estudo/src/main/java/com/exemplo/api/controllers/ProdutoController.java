38a1de1bf1868a425c476a40821a038c6e4207cb
Lucas Passos Vanderlinde <lucaspvanderlinde@hotmail.com>
2020-11-17 10:52:52 -0300

    Desenvolvido o delete e o atualizar nos controllers.

--- back-end/estudo/src/main/java/com/exemplo/api/controllers/ProdutoController.java
+++ back-end/estudo/src/main/java/com/exemplo/api/controllers/ProdutoController.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.AfterDomainEventPublication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,6 +59,31 @@ public class ProdutoController {
 	@PostMapping
 	public ResponseEntity<Produto> cadastrar(@RequestBody Produto produto) {
 		produto = produtoService.cadastrar(produto);
+		return ResponseEntity.ok().body(produto);
+	}
+
+	/**
+	 * Deleta um produto do banco.
+	 * 
+	 * @param id
+	 * @return
+	 */
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		produtoService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	/**
+	 * Altera um produto do banco pelo id.
+	 * 
+	 * @param id
+	 * @param produto
+	 * @return
+	 */
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Produto> atualizar(@PathVariable Long id, @RequestBody Produto produto) {
+		produto = produtoService.atualizar(id, produto);
 		return ResponseEntity.ok().body(produto);
 	}
 
