1bb9004d421788aa3261c0cf8e4a348e9c6e243e
Guilherme Araujo <gui.araujo1902@gmail.com>
2020-11-18 21:57:18 -0300

    Front-end: criação do componente produto-listar

---
+++ front-end/estudo/src/app/shared/components/produto/produto-cadastrar/produto-cadastrar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-cadastrar',
+  templateUrl: './produto-cadastrar.component.html',
+  styleUrls: ['./produto-cadastrar.component.css']
+})
+export class ProdutoCadastrarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
