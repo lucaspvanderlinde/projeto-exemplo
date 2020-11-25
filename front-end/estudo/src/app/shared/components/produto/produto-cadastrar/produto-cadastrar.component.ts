5785e7764de162e85ab6bcbedb924ba1b97a499c
Guilherme Araujo <gui.araujo1902@gmail.com>
2020-11-24 22:56:09 -0300

    Front-end: criação dos métidos de Cadastrar Produto.

--- front-end/estudo/src/app/shared/components/produto/produto-cadastrar/produto-cadastrar.component.ts
+++ front-end/estudo/src/app/shared/components/produto/produto-cadastrar/produto-cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProdutoService } from 'src/app/core/services/produto/produto.service';
+import { Produto } from 'src/app/shared/models/Produto';
 
 @Component({
   selector: 'app-produto-cadastrar',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProdutoCadastrarComponent implements OnInit {
 
-  constructor() { }
+  public produto: Produto = new Produto();  
+  dateFormat = 'dd/MM/yyyy';
+
+  constructor(private produtoService: ProdutoService, private router: Router) { }
 
   ngOnInit(): void {
+
+    this.produto.quantidade = 0;
+
   }
+
+  public onChange(result: Date): void {
+    console.log('onChange: ', result);
+  }
+
+  public onSubmit() {
+    this.produtoService.cadastrar(this.produto)
+      .subscribe(data => {
+        console.log(data)
+        this.produto = new Produto();
+        this.irParaLista();
+      },
+        error => console.log(error)
+      );
+  }
+
+  public irParaLista() {
+    this.router.navigate(['']);
+  }
+
 
 }
--- front-end/estudo/src/app/shared/modules/shared.module.ts
+++ front-end/estudo/src/app/shared/modules/shared.module.ts
@@ -6,6 +6,11 @@ import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzInputNumberModule } from 'ng-zorro-antd/input-number';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzButtonModule } from 'ng-zorro-antd/button';
 
 @NgModule({
     exports: [
@@ -15,7 +20,12 @@ import { NzTableModule } from 'ng-zorro-antd/table';
         NzLayoutModule,
         NzBreadCrumbModule,
         NzMenuModule,
-        NzTableModule
+        NzTableModule,
+        NzFormModule,
+        NzInputModule,
+        NzInputNumberModule,
+        NzDatePickerModule,
+        NzButtonModule
     ]
 })
 export class SharedModule{}
